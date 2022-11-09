7ceaa4518e38cad7cf7f65f744b519f0b69fbf0e
chico ferreira <36338391+chicoferreira@users.noreply.github.com>
2022-11-09 11:28:13 +0000

    fix: Makefile working without warnings

--- trabalho-pratico/include/file_util.h
+++ trabalho-pratico/include/file_util.h
@@ -2,7 +2,7 @@
 
 #include <stdio.h>
 
-FILE *open_file_folder(char *folder_path, char *file_name);
+FILE *open_file_folder(const char *const folder_path, const char *const file_name);
 
-FILE *open_file(char *file_name);
+FILE *open_file(const char *const file_name);
 
