47f989863c316d4701222c2966e6c04b63c8bdd4
chico ferreira <36338391+chicoferreira@users.noreply.github.com>
2023-01-01 03:47:59 +0000

    test: Add unit tests for date

--- trabalho-pratico/test/main.c
+++ trabalho-pratico/test/main.c
@@ -146,12 +146,60 @@ void test_date_parse_and_encoding(void) {
     }
 }
 
+void test_date_compare(void) {
+    Date date1 = create_date(1, 1, 2000);
+    Date date3 = create_date(1, 1, 2001);
+    Date date4 = create_date(1, 2, 2000);
+    Date date5 = create_date(2, 1, 2000);
+    Date date6 = create_date(8, 5, 1999);
+
+    if (date_compare(date1, date1) != 0) {
+        g_test_fail_printf("Date1 (01/01/2000) should've been equal to itself");
+    }
+    if (date_compare(date1, date3) >= 0) {
+        g_test_fail_printf("Date1 (01/01/2000) should've been before Date3 (01/01/2001)");
+    }
+    if (date_compare(date1, date4) >= 0) {
+        g_test_fail_printf("Date1 (01/01/2000) should've been before Date4 (01/02/2000)");
+    }
+    if (date_compare(date5, date1) <= 0) {
+        g_test_fail_printf("Date5 (02/01/2000) should've been after Date1 (01/01/2000)");
+    }
+    if (date_compare(date6, date1) >= 0) {
+        g_test_fail_printf("Date6 (08/05/1999) should've been before Date1 (01/01/2000)");
+    }
+}
+
+
+// 9/10/2022
+void test_date_age(void) {
+    Date date1 = create_date(1, 1, 2000);
+    Date date2 = create_date(7, 2, 2005);
+    Date date3 = create_date(9, 10, 2022);
+    Date date4 = create_date(9, 10, 2021);
+
+    if (get_age(date1) != 22) {
+        g_test_fail_printf("Age should've been 22 for date1 (01/01/2000) but is %d", get_age(date1));
+    }
+    if (get_age(date2) != 17) {
+        g_test_fail_printf("Age should've been 17 for date2 (07/02/2005) but is %d", get_age(date2));
+    }
+    if (get_age(date3) != 0) {
+        g_test_fail_printf("Age should've been 0 for date3 (09/10/2022) but is %d", get_age(date3));
+    }
+    if (get_age(date4) != 1) {
+        g_test_fail_printf("Age should've been 1 for date4 (09/10/2021) but is %d", get_age(date4));
+    }
+}
+
 int main(int argc, char *argv[]) {
     g_test_init(&argc, &argv, NULL);
 
     g_test_set_nonfatal_assertions();
 
     g_test_add_func("/assert_date_correct_parse_and_encoding", test_date_parse_and_encoding);
+    g_test_add_func("/assert_date_correct_compare", test_date_compare);
+    g_test_add_func("/assert_date_correct_age", test_date_age);
     g_test_add_func("/assert_invalid_csv_loads_nothing_large", assert_invalid_csv_loads_nothing_large);
     g_test_add_func("/assert_invalid_csv_loads_nothing_regular", assert_invalid_csv_loads_nothing_regular);
     g_test_add_func("/assert_valid_csv_loads_everything", assert_valid_csv_loads_everything_regular);
