09a33eeaebf3a71860a8c2bc17874911f3727e98
Júlio Pinto <pinto.julioj03@gmail.com>
2023-01-25 16:09:02 +0000

    report: add performance table

--- trabalho-pratico/src/main.c
+++ trabalho-pratico/src/main.c
@@ -33,7 +33,7 @@ int main(int argc, char **argv) {
     g_ptr_array_free(program_args, TRUE);
     free_program_flags(program_flags);
 
-    log_info("Total runtime:          %lf seconds\n", g_timer_elapsed(global_timer, NULL));
+    log_info("Total runtime:          " TERMINAL_YELLOW_BOLD "%lf seconds\n" TERMINAL_RESET, g_timer_elapsed(global_timer, NULL));
 
     return result;
 }
